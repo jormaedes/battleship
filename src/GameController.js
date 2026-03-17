339e2fc32f6b9fb5b0d165a528d02de12ccd737f
Jormaedes Luis <jormahed@gmail.com>
2026-03-17 16:48:53 +0100

    fix: add export class GameController

--- src/GameController.js
+++ src/GameController.js
@@ -1,9 +1,26 @@
 import { Player } from "./Player.js";
 
-class GameController {
+export class GameController {
 	constructor() {
 		this.playerBoard = new Player('human');
 		this.computerBoard = new Player('computer');
 		this.winner = null;
+	}
+
+	isGameOver() {
+		return (this.playerBoard.board.allSunk() || this.computerBoard.board.allSunk());
+	}
+
+	handleAttack([row, col]) {
+		this.playerBoard.attack(this.computerBoard.board, [row, col]);
+		if (this.isGameOver()) {
+			this.winner = 'human';
+			return ;
+		}
+		this.computerBoard.randomAttack(this.playerBoard.board);
+		if (this.isGameOver()) {
+			this.winner = 'computer';
+			return ;
+		}
 	}
 }
