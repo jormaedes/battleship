46877aca98edec7e1865d8d4fcb4c5b501aa4dcf
Jormaedes Luis <jormahed@gmail.com>
2026-03-18 13:30:55 +0100

    feat: add construtor

--- src/DOMController.js
+++ src/DOMController.js
@@ -2,30 +2,40 @@ import { GameController } from './GameController.js';
 import { Ship } from './Ship.js';
 
 const SHIPS = [
-  { name: 'Carrier', length: 5 },
-  { name: 'Battleship', length: 4 },
-  { name: 'Destroyer', length: 3 },
-  { name: 'Submarine', length: 3 },
-  { name: 'Patrol Boat', length: 2 },
+	{ name: 'Carrier', length: 5 },
+	{ name: 'Battleship', length: 4 },
+	{ name: 'Destroyer', length: 3 },
+	{ name: 'Submarine', length: 3 },
+	{ name: 'Patrol Boat', length: 2 },
 ];
 
 export class DOMController {
-  constructor(gameController) {
-    this.game = gameController;
-    this.app = document.getElementById('app');
-    this.playerName = '';
-  }
+	constructor(gameController) {
+		this.game = gameController;
+		this.app = document.getElementById('app');
+		this.playerName = '';
+		this.shipsToPlace = [];
+		this.currentShipIndex = 0;
+		this.orientation = 'horizontal';
+		this.tempGrid = null;
+		this.messageEl = null;
+		this.startBtn = null;
+		this.shipListEl = null;
+		this.placementGrid = null;
+		this.playerBoardEl = null;
+		this.computerBoardEl = null;
+	}
 
-  init() {
-    this.renderSetupScreen();
-  }
+	init() {
+		this.renderSetupScreen();
+	}
 
-  renderSetupScreen() {  }
-  renderPlacementScreen() {  }
-  renderGameScreen() {  }
+	renderSetupScreen() { }
+	renderPlacementScreen() { }
+	renderGameScreen() { }
 
-  // helpers
-  renderBoard(gameboard, container, isEnemy = false) {  }
-  updateMessage(text) {  }
-  clearApp() { this.app.innerHTML = ''; }
+	// helpers
+	renderBoard(gameboard, container, isEnemy = false) { }
+	updateMessage(text) { }
+	clearApp() { this.app.innerHTML = ''; }
 }
